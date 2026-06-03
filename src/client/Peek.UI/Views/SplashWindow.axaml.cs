0cc8e8dcb7b44e3eeb7c3694bfd6d885fdfd38ca
Easley <nevermore.wd@me.com>
2026-06-03 17:24:23 +0200

    improve splash effect

--- src/client/Peek.UI/Views/SplashWindow.axaml.cs
+++ src/client/Peek.UI/Views/SplashWindow.axaml.cs
@@ -1,5 +1,7 @@
 using AsyncNavigation.Abstractions;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Media;
 
 namespace Peek.UI.Views;
 
@@ -8,5 +10,21 @@ public partial class SplashWindow : Window, IView, IDialogWindow
     public SplashWindow()
     {
         InitializeComponent();
+    }
+    protected override void OnLoaded(RoutedEventArgs e)
+    {
+        base.OnLoaded(e);
+        MainCrt.EnableScanlines = true;
+        MainCrt.ScanlineColor = Color.FromArgb(255, 0, 230, 60);
+        MainCrt.ScanlineOpacity = 0.18;
+        MainCrt.ScanlineSpacing = 3.0;
+        MainCrt.EnableScanlineAnimation = true;
+        MainCrt.ScanlineAnimSpeed = 20.0;
+        MainCrt.EnableScanBeam = true;
+        MainCrt.ScanBeamHeight = 60.0;
+        MainCrt.EnableNoise = false;
+        MainCrt.EnableVignette = true;
+        MainCrt.VignetteIntensity = 0.55;
+        MainCrt.EnableFlicker = false;
     }
 }
